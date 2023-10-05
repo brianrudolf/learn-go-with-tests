c5c2c1fb4eb6561053a69ede536b5b701a186295
brianrudolf <white.bjrw@gmail.com>
2023-10-05 14:14:56 -0400

    Add multiple languages to 'hello-world'

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,15 +3,35 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const germanHelloPrefix = "Guten tag, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix + name
+
+	var prefix string
+
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "English":
+		prefix = englishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	case "German":
+		prefix = germanHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	message := prefix + name
+	return message
 }
 
 func main() {
-	fmt.Println(Hello("Brian"))
+	fmt.Println(Hello("Brian", ""))
 }
 
